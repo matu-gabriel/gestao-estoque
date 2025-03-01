2e4861f7b9f837b6b458600ec96777b8a1ea7cbe
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2025-03-01 13:04:49 -0300

    altera nome da tabela de "product" para "products" nas migrações de estoque

--- backend/src/database/migrations/20250228142828-create-stock-out.js
+++ backend/src/database/migrations/20250228142828-create-stock-out.js
@@ -13,7 +13,7 @@ module.exports = {
       product_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: { model: "product", key: "id" },
+        references: { model: "products", key: "id" },
         onUpdate: "CASCADE",
         onDelete: "CASCADE",
       },
