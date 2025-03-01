074bf30900a4dc2d37b99c411f7df6a71b323a63
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2025-03-01 10:45:03 -0300

    adiciona migrações para criar tabelas de entrada e saída de estoque, além da tabela de usuários

---
+++ backend/src/database/migrations/20250228140250-create-stock-entry.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("stock_entry", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "product", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      entry_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("stock_entry");
+  },
+};
