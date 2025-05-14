7c1238a085007ead7f12dca949013b2966b68fd2
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2025-05-13 23:25:09 -0300

    atualiza configurações de conexão com o banco de dados para usar variáveis de ambiente

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "postgres",
-  database: "gestao-estoque",
-  port: 5432,
+  host: process.env.DB_HOST || "db",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   define: {
     timestamps: true,
     underscored: true,
